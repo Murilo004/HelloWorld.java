3ec1b9d710fe9d201a24edb1d2debf831dc32cbc
Murilo Rálio <muriloralio8@gmail.com>
2025-10-23 19:09:36 -0300

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+package section13;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Word!!!");
+		
+		System.out.println("My name is Murilo!!");
+		
+	}
+	
+}
